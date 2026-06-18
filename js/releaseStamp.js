d1a26b156af422dc42be1d723f878a6fc38cd6f0
Cinaedvsstudios <cinaedvsstudios@gmail.com>
2026-06-18 16:52:20 +0200

    Add visible Actarium release stamp v3.15.0

---
+++ js/releaseStamp.js
@@ -0,0 +1,14 @@
+(() => {
+  const RELEASE = 'v3.15.0';
+  const apply = () => {
+    document.querySelectorAll('.actarium-version').forEach(node => {
+      node.textContent = RELEASE;
+      node.dataset.release = RELEASE;
+      node.title = `Actarium release ${RELEASE}`;
+    });
+    document.documentElement.dataset.actariumRelease = RELEASE;
+  };
+
+  new MutationObserver(apply).observe(document.documentElement, { childList: true, subtree: true });
+  apply();
+})();
